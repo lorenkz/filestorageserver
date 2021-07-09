95e94bd6194b1c879cc97745ba5860c808fb66d0
Lorenzo <l.stefanelli@protonmail.com>
2021-07-09 16:43:58 +0200

    added a header with server configuration default values

---
+++ include/fss_defaults.h
@@ -0,0 +1,14 @@
+#ifndef FSS_DEFAULTS_H
+#define FSS_DEFAULTS_H
+
+/**
+ * The following macros specify the default values for the server configuration.
+ */
+
+#define DEF_SOCKET_NAME "tmp/filestorageserver.sk"
+#define DEF_WORKER_POOL_SIZE 5
+#define DEF_STORAGE_MAX_FILE_NUMBER 1000
+#define DEF_STORAGE_MAX_SIZE 128
+#define DEF_BACKLOG 32
+
+#endif
